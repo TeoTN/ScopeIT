086bbeb0d22797d49f4d9beb91bff9ff84ab5e7e
Piotr Staniów <staniowp@gmail.com>
2015-11-18 00:32:07 +0100

    Profile form

--- src/static/js/accounts/accounts.controllers.js
+++ src/static/js/accounts/accounts.controllers.js
@@ -28,15 +28,16 @@
         };
     }
 
-    EntityController.$inject = ['$scope', '$cookies', 'Profile', 'Entity'];
+    EntityController.$inject = ['$scope', '$cookies', 'Entity', 'Skills'];
 
-    function EntityController($scope, $cookies, $profile, $entity) {
+    function EntityController($scope, $cookies,  $entity, $skills) {
         var vm = this;
         var username = $cookies.get('username');
         vm.remove = function(entity) {
             $entity.remove(username, entity);
             entity.deleted = true;
         };
+        vm.skill_levels = $skills.skill_levels;
     }
 
     EntityListController.$inject = ['$scope', '$cookies', 'Profile', 'Entity'];
@@ -57,10 +58,55 @@
             });
 
         $scope.can_add_entity = function() {
-            return $scope.is_employer || $scope.entityList.length == 0;
+            return $scope.is_employer || $scope.entity_list.length == 0;
         }
     }
 
-    EntityFormController.$inject = ['$scope'];
-    function EntityFormController($scope) {}
+    EntityFormController.$inject = ['$scope', '$cookies', '$window', 'Entity', 'Skills'];
+    function EntityFormController($scope, $cookies, $window, $entity, $skills) {
+        var vm = this;
+        $scope.new_entity = {
+            'skills': [],
+        };
+        $scope.new_skill = {};
+
+        vm.submit = function(valid) {
+            var username = $cookies.get('username');
+            $entity
+                .create(username, $scope.new_entity)
+                .then(function(response) {
+                    $window.location.reload();
+                }, function(error) {
+                    console.log(error);
+                })
+        };
+
+        vm.add_skill = function() {
+            if (!$scope.newEntity.$valid) return;
+            $scope.new_entity.skills.push($scope.new_skill);
+            $scope.new_skill = {};
+        };
+
+        vm.rm_skill = function(skill) {
+            var index = $scope.new_entity.skills.indexOf(skill);
+            if (index != -1) {
+                $scope.new_entity.skills.splice(index, 1);
+            }
+        }
+
+        vm.skill_types = {
+            'LNG': 'Programming language',
+            'FRW': 'Framework',
+            'LIB': 'Library',
+            'OS': 'Operating system',
+            'TOOL': 'Tool / application',
+            'TECH': 'Technology'
+        };
+
+        vm.skill_levels = $skills.skill_levels;
+
+        $skills.list().then(function(response){
+            $scope.skills = response;
+        });
+    }
 })();
---
+++ src/static/js/accounts/accounts.services.skills.js
@@ -0,0 +1,71 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('scopeit.accounts.services')
+        .factory('Skills', Skills);
+
+    Skills.$inject = ['$http', 'apiUrl'];
+
+    function Skills($http, apiUrl) {
+        var base_url = 'skills/';
+        var skill_levels = {
+            '5': 'Expert',
+            '4': 'Advanced',
+            '3': 'Intermediate',
+            '2': 'Beginner'
+        };
+
+        var Skills = {
+            create: create,
+            retrieve: retrieve,
+            update: update,
+            remove: remove,
+            list: list,
+            skill_levels: skill_levels
+        };
+        return Skills;
+
+        function retrieve(pk) {
+            var request = {
+                url: apiUrl + base_url + pk,
+                method: 'GET',
+            };
+            return $http(request);
+        }
+
+        function list() {
+            var request = {
+                url: apiUrl + base_url,
+                method: 'GET',
+            };
+            return $http(request);
+        }
+
+        function create(skill) {
+            var request = {
+                url: apiUrl + base_url,
+                method: 'POST',
+                data: skill
+            };
+            return $http(request);
+        }
+
+        function update(skill) {
+            var request = {
+                url: apiUrl + base_url + skill.pk,
+                method: 'PATCH',
+                data: skill
+            };
+            return $http(request);
+        }
+
+        function remove(skill) {
+            var request = {
+                url: apiUrl + base_url + skill.pk,
+                method: 'DELETE',
+            };
+            return $http(request);
+        }
+    }
+})();
