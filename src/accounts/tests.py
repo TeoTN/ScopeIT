63fda9107cce498afe18ebf66124b4a26cce2456
Piotr Staniów <staniowp@gmail.com>
2015-11-30 21:29:02 +0000

    Comparator tests

--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -1,3 +1,18 @@
-# from django.test import TestCase
+from django.test import TestCase
+from accounts.stable_matching.matcher import *
+from accounts.models import *
 
-# Create your tests here.
+
+class TestComparator(TestCase):
+    fixtures = ['dump']
+    def setUp(self):
+        self.entities = Entity.objects.all()
+        self.comparator = Comparator()
+
+    def test_should_make_comparison(self):
+        job_offer = ProposingRole(self.entities[2])
+        applicant = AcceptingRole(self.entities[3])
+        proximity = self.comparator.get_proximity(job_offer, applicant)
+        print(proximity)
+        self.assertTrue(type(proximity) == type(0.0))
+
